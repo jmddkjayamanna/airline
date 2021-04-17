3e5faefa66864ee04617888c5c39ef30e0d841ba
rkskrajapakshe <74091350+rkskrajapakshe@users.noreply.github.com>
2021-04-17 14:22:35 +0530

    servlet check

---
+++ web/Javascript/HomeValidation.js
@@ -0,0 +1,16 @@
+
+function checkflightvali()
+{
+    if(document.checkflight.flyfrom.value==0)
+    {
+        alert("Please Enter Departing Airport or City");
+    }
+    if(document.checkflight.flyto.value==0)
+    {
+        alert("Please Enter DDestination Airport or City");
+    }
+    
+    
+       
+    
+}
